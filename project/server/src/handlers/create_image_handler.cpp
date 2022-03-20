8c87057d1dabca872b91f97643dbfec740d7862d
dmitrii-artuhov <artuhovdmitrii@gmail.com>
2022-03-20 14:08:18 +0300

    add saving blank image to file

--- project/server/src/handlers/create_image_handler.cpp
+++ project/server/src/handlers/create_image_handler.cpp
@@ -1,6 +1,9 @@
 #include "handlers/create_image_handler.h"
 #include "Poco/Net/HTTPServerResponse.h"
+#include "Poco/UUIDGenerator.h"
+#include "charta/image.h"
 #include "utils/utils.h"
+#include <exception>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,14 +13,28 @@ using namespace Poco::Net;
 
 void CreateImageHandler::handleRequest(HTTPServerRequest &request,
                                        HTTPServerResponse &response) {
-  response.setStatus(HTTPResponse::HTTP_CREATED);
-  Poco::URI::QueryParameters params = uri_.getQueryParameters();
+  try {
+    response.setStatus(HTTPResponse::HTTP_CREATED);
+    Poco::URI::QueryParameters params = uri_.getQueryParameters();
 
-  std::string response_text = "Create Image:\n";
-  for (auto &[key, value] : params) {
-    response_text += key + " : " + value + "\n";
+    int width, height;
+    std::string response_text = "Create Image:\n";
+    for (auto &[key, value] : params) {
+      response_text += key + " : " + value + "\n";
+      if (key == "width") {
+        width = std::stoi(value);
+      } else if (key == "height") {
+        height = std::stoi(value);
+      }
+    }
+
+    std::string image_name = utils::generateUUID().toString() + ".bmp";
+    BMPImage image = BMPImage(width, height, image_name);
+    image.SaveToFile("");
+
+    response.setContentType("text/plain");
+    response.send() << response_text;
+  } catch (const std::exception &err) {
+    std::cerr << err.what() << std::endl;
   }
-
-  response.setContentType("text/plain");
-  response.send() << response_text;
 }
--- project/server/src/utils/utils.h
+++ project/server/src/utils/utils.h
@@ -2,6 +2,8 @@
 #ifndef TEST_
 #define TEST_
 #include "Poco/URI.h"
+#include "Poco/UUID.h"
+#include "Poco/UUIDGenerator.h"
 #include <string>
 
 namespace charta::utils {
@@ -10,5 +12,11 @@ inline std::string GetPathSegmentValue(Poco::URI &uri, int index = 0) {
   uri.getPathSegments(pathComponents);
   return pathComponents[index];
 }
+
+inline static Poco::UUIDGenerator getUUIDGenerator() {
+  return Poco::UUIDGenerator();
+}
+
+inline Poco::UUID generateUUID() { return getUUIDGenerator().create(); }
 } // namespace charta::utils
 #endif
